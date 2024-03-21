cf3ec6afc903ab02dc4eda0ea6b1e6c20dc564fe
robemike <robemike1135@gmail.com>
2024-03-21 18:10:16 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Mike Robe '
+let welcomeMessage = 'Welcome to Flatbook!'
+welcomeMessage =  'Welcome to Flatbook, '
+welcomeMessage =  'Welcome to Flatbook, Mike Robe '
+welcomeMessage =  'Welcome to Flatbook, Mike Robe +!'
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK\, MIKE ROBE ';
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK\, MIKE ROBE +!';
+let shortGreeting = 'Welcome, ';
+shortGreeting = 'Welcome\, M ';
+shortGreeting = 'Welcome\, M +!';
+
+
